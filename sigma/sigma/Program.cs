3a2a4e769bb8f5c324c146ada9fbf3785908e8ee
LaykDimon <laykdimon278@gmail.com>
2020-11-22 00:47:33 +0200

    project created

---
+++ sigma/sigma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("bye");
+        }
+    }
+}
